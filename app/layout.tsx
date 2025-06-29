b98faf86a2d4246d9a0757b1134a03e7fd013528
Lakshya <165297915+laksh-ya@users.noreply.github.com>
2025-06-29 19:09:14 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Roast Your Base',
+  description: 'Roast your COC Base!!',
+  generator: 'lakshya',
 }
 
 export default function RootLayout({
